e2e63a5e400946b112db62f9e39c496b80a4c27c
Omri Mor <>
2018-04-13 14:04:32 +0300

    setting up action-types

---
+++ src/consts/actionTypes.tsx
@@ -0,0 +1,9 @@
+export const YOU_TUBE_API_REQUEST: string = 'YOU_TUBE_API_REQUEST';
+
+export const ADD_VIDEO_ITEM: string = 'ADD_VIDEO_ITEM';
+export const SET_NEXT_VIDEO_ITEM: string = 'SET_NEXT_VIDEO_ITEM';
+
+export const START_NETWORK: string = 'START_NETWORK';
+export const END_NETWORK: string = 'END_NETWORK';
+
+export const SET_NETWORK_ERRORS = 'SET_NETWORK_ERRORS';
---
+++ src/consts/types.tsx
@@ -0,0 +1,8 @@
+
+export interface VideoItem {
+  id: string;
+  title: string;
+  duration: string;
+  snippet: any;
+  contentDetails: any;
+}
