07854f6e8c7a3fe206a6d2faca05efae74b042d8
Omri Mor <>
2018-04-13 14:07:25 +0300

    install packages

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+This project was bootsrapped with [TypeScript React Starter]
+(https://github.com/Microsoft/TypeScript-React-Starter).
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Below you will find some information on how to perform common tasks.<br>
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store';
+
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
